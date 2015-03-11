b3399b723a5e02b99009202e75217cc4f6d397ab
robroooh <robroo.pc@gmail.com>
2015-03-11 20:20:12 +0700

    done with the home page

--- Resources/app.js
+++ Resources/app.js
@@ -2,8 +2,11 @@ var win = Ti.UI.createWindow({
 	backgroundColor : "#000"
 });
 
-var image = Ti.UI.createImageView({
-  image:'/images/pornhub-logo.png'
+var logo = Ti.UI.createImageView({
+	image: '/images/pornhub-logo.jpg',
+	width:300,
+	height:300,
+	top:30
 });
 
 
@@ -11,8 +14,27 @@ var label = Ti.UI.createLabel({
 	text : "Landmark",
 	width: "auto",
 	height : "auto",
-	color: "#FFF"
+	color: "#FFF",
+	top:300,
+	font:{
+		fontFamily:'Arial',
+		fontWeight:'bold',
+		fontSize:40
+		}
 });
+
+var button = Ti.UI.createButton({
+	 title: "Help The World!",
+	 height: 40,
+	 width: 200,
+	 top:450
+});
+
+// button.addEventListener('click', function() {
+// 
+	// //prepare to move the window
+// });
+//  
 
 
 /*var count = 0;
@@ -22,8 +44,8 @@ Ti.Gesture.addEventListener("shake", function(e){
 	label.text = count;
 });*/
 
-win.add(image);
-
+win.add(logo);
 win.add(label);
+win.add(button);
 
 win.open();
